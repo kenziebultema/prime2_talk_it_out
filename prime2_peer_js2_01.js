6f7e1cce88e115acdc272abdaf0318086c7f18b2
rileyharrison <rileyharrison@gmail.com>
2016-02-23 17:03:37 -0600

    update project

--- prime2_peer_js2_01.js
+++ prime2_peer_js2_01.js
@@ -31,6 +31,34 @@ for(i = myArray.length - 1; i >= 0; i--){
 newstr = parseInt(newstr);
 value = parseInt(value);
 
+value += newstr;
+
+if (value>1954){
+	value = 56;
+
+  } else if (value == 1852){
+  	value = 19;
+
+} else {
+	value = 91;
+}
+
+i = 11;
+while (i>0){
+	value++;
+	i--;
+}
+
+var cutstring = function(val){
+	val.toString();
+	var stringcut = val.substring(1,val.length);
+console.log(stringcut);
+
+}
+cutstring(123);
+
+//console.log("stringcut ="+stringcut);
 console.log(newstr);
 console.log(myArray);
 console.log(value);
+
